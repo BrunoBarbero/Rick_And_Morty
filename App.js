09aa0066f793ccd55f59973ece2033a0fe533e1c
Bruno Barbero <colo_barbero16@hotmail.com>
2023-04-04 11:07:31 -0300

    segundo commit

---
+++ App.js
@@ -0,0 +1,37 @@
+import './App.css';
+import Cards from './components/Cards.jsx';
+import { useState } from 'react';
+import Nav from './components/nav';
+import axios from 'axios';
+
+function App() {
+
+   const [characters, setCharacters] = useState([])  
+
+   const onSearch =(id) => {
+      axios(`https://rickandmortyapi.com/api/character/${id}`)
+      .then(({ data }) => {
+         if (data.name) {
+            setCharacters((oldChars) => [...oldChars, data]);
+         } else {
+            window.alert('¡No hay personajes con este ID!');
+         }
+      });
+   }
+   
+   const onClose = (id)=>{
+      setCharacters(
+         characters.filter(character => character.id !== Number(id))
+   )}
+
+   return (
+      <div className='App'>
+         
+         <Nav onSearch={onSearch} />
+         <Cards onClose={onClose} characters={characters} />
+        
+      </div>
+   );
+}
+
+export default App;
